7eaf59df8c6b3d3e2331c90ae982a49025d522e0
BryceTheGrand <whatkindofbunny@gmail.com>
2019-05-04 16:18:51 -0400

    Fixed spacing

--- sketch.js
+++ sketch.js
@@ -4,127 +4,127 @@ var endWin = 0;
 
 
 function setup() {
-  createCanvas(600, 600);
+    createCanvas(600, 600);
 
-  for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
 
-    grid.push(new TicTacToe());
+        grid.push(new TicTacToe());
 
-  }
+    }
 
-  textAlign(CENTER);
-  textSize(48);
+    textAlign(CENTER);
+    textSize(48);
 }
 
 function draw() {
 
-  background(5);
-  drawTicTacToe();
+    background(5);
+    drawTicTacToe();
 
 
-  fill(255, 100);
-  noStroke();
-  text((turn == 1 || turn == 0) ? "X" : "O", mouseX, mouseY + 20);
+    fill(255, 100);
+    noStroke();
+    text((turn == 1 || turn == 0) ? "X" : "O", mouseX, mouseY + 20);
 
-  noFill();
-  stroke(255);
+    noFill();
+    stroke(255);
 
-  for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
 
-    if (grid[i].active) {
+        if (grid[i].active) {
 
-      rect((i % 3) * width / 3, (floor(i / 3)) * height / 3, (width / 3) - 1, (height / 3) - 1);
+            rect((i % 3) * width / 3, (floor(i / 3)) * height / 3, (width / 3) - 1, (height / 3) - 1);
 
-    }
-
-  }
-
-  fill(255);
-  noStroke();
-
-  drawXOs();
-
-  textSize(200);
-
-  for (let i = 0; i < 9; i++) {
-
-    if (grid[i].winner != 0) {
-
-      text(((grid[i].winner == 1) ? "X" : "O"), (i % 3) * width / 3 + width / 6, (floor(i / 3)) * height / 3 + height / 3.5);
+        }
 
     }
 
-  }
+    fill(255);
+    noStroke();
 
-  textSize(48);
+    drawXOs();
 
+    textSize(200);
 
-  if (endWin != 0) {
+    for (let i = 0; i < 9; i++) {
 
-    textSize(width);
-    text((endWin == 1 ? "X" : "O"), width / 2, height / 2 + 210);
+        if (grid[i].winner != 0) {
+
+            text(((grid[i].winner == 1) ? "X" : "O"), (i % 3) * width / 3 + width / 6, (floor(i / 3)) * height / 3 + height / 3.5);
+
+        }
+
+    }
+
     textSize(48);
-    noLoop();
 
-  }
+
+    if (endWin != 0) {
+
+        textSize(width);
+        text((endWin == 1 ? "X" : "O"), width / 2, height / 2 + 210);
+        textSize(48);
+        noLoop();
+
+    }
 
 
 }
 
 
 function drawXOs() {
-  
- for (let index = 0; index < 9; index++) {
+
+    for (let index = 0; index < 9; index++) {
 
 
-    for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) {
 
-      for (let j = 0; j < 3; j++) {
+            for (let j = 0; j < 3; j++) {
 
-        if (grid[index].grid[i][j] == 1) {
+                if (grid[index].grid[i][j] == 1) {
 
-          text("X", (index % 3) * width / 3 + j * width / 9 + 35, (floor(index / 3)) * height / 3 + i * height / 9 + 48);
+                    text("X", (index % 3) * width / 3 + j * width / 9 + 35, (floor(index / 3)) * height / 3 + i * height / 9 + 48);
 
-        } else if (grid[index].grid[i][j] == -1) {
+                } else if (grid[index].grid[i][j] == -1) {
 
-          text("O", (index % 3) * width / 3 + j * width / 9 + 35, (floor(index / 3)) * height / 3 + i * height / 9 + 48);
+                    text("O", (index % 3) * width / 3 + j * width / 9 + 35, (floor(index / 3)) * height / 3 + i * height / 9 + 48);
+
+                }
+
+            }
 
         }
 
-      }
 
     }
 
-
-  } 
-  
 }
 
 
 function drawTicTacToe() {
 
-  stroke(255);
+    stroke(255);
 
-  for (let row = 0; row < 3; row++) {
+    for (let row = 0; row < 3; row++) {
 
-    for (let col = 0; col < 3; col++) {
+        for (let col = 0; col < 3; col++) {
 
-      for (let x = 1; x < 3; x++) {
+            for (let x = 1; x < 3; x++) {
 
-        line(x * width / 9 + row * width / 3, 20 + col * height / 3, x * width / 9 + row * width / 3, height / 3 - 20 + col * height / 3);
+                line(x * width / 9 + row * width / 3, 20 + col * height / 3, x * width / 9 + row * width / 3, height / 3 - 20 + col * height / 3);
 
-      }
+            }
 
-      for (let y = 1; y < 3; y++) {
+            for (let y = 1; y < 3; y++) {
 
-        line(20 + row * width / 3, y * height / 9 + col * height / 3, width / 3 - 20 + row * width / 3, y * height / 9 + col * height / 3);
+                line(20 + row * width / 3, y * height / 9 + col * height / 3, width / 3 - 20 + row * width / 3, y * height / 9 + col * height / 3);
 
-      }
+            }
+
+        }
+
 
     }
-
-
-  }
 
 
 }
@@ -132,71 +132,71 @@ function drawTicTacToe() {
 
 function mouseClicked() {
 
-  let colBoard = floor(mouseX / width * 3);
-  let rowBoard = floor(mouseY / height * 3);
+    let colBoard = floor(mouseX / width * 3);
+    let rowBoard = floor(mouseY / height * 3);
 
-  let col = floor(mouseX / width * 9);
-  let row = floor(mouseY / height * 9);
-  
+    let col = floor(mouseX / width * 9);
+    let row = floor(mouseY / height * 9);
 
-  if (turn == 1 || turn == 0) {
 
-    if ((grid[colBoard + rowBoard * 3].active || turn == 0) && grid[colBoard + rowBoard * 3].grid[row % 3][col % 3] == 0 && grid[colBoard + rowBoard * 3].winner == 0) {
+    if (turn == 1 || turn == 0) {
 
-      grid[colBoard + rowBoard * 3].grid[row % 3][col % 3] = 1;
-      grid[colBoard + rowBoard * 3].active = false;
+        if ((grid[colBoard + rowBoard * 3].active || turn == 0) && grid[colBoard + rowBoard * 3].grid[row % 3][col % 3] == 0 && grid[colBoard + rowBoard * 3].winner == 0) {
 
-      if (grid[col % 3 + (row % 3) * 3].winner == 0) {
+            grid[colBoard + rowBoard * 3].grid[row % 3][col % 3] = 1;
+            grid[colBoard + rowBoard * 3].active = false;
 
-        grid[col % 3 + (row % 3) * 3].active = true;
-        turn = 2;
+            if (grid[col % 3 + (row % 3) * 3].winner == 0) {
 
-      } else {
+                grid[col % 3 + (row % 3) * 3].active = true;
+                turn = 2;
 
-        turn = 3;
+            } else {
 
-      }
+                turn = 3;
+
+            }
+
+        }
+
+    } else if (turn == 2 || turn == 3) {
+
+        if ((grid[colBoard + rowBoard * 3].active || turn == 3) && grid[colBoard + rowBoard * 3].grid[row % 3][col % 3] == 0 && grid[colBoard + rowBoard * 3].winner == 0) {
+
+            grid[(colBoard + rowBoard * 3)].grid[row % 3][col % 3] = -1;
+            grid[colBoard + rowBoard * 3].active = false;
+
+            if (grid[col % 3 + (row % 3) * 3].winner == 0) {
+
+                grid[col % 3 + (row % 3) * 3].active = true;
+                turn = 1;
+
+            } else {
+
+                turn = 0;
+
+            }
+
+        }
 
     }
 
-  } else if (turn == 2 || turn == 3) {
+    checkTriplet(colBoard + rowBoard * 3);
 
-    if ((grid[colBoard + rowBoard * 3].active || turn == 3) && grid[colBoard + rowBoard * 3].grid[row % 3][col % 3] == 0 && grid[colBoard + rowBoard * 3].winner == 0) {
+    let count = 0;
 
-      grid[(colBoard + rowBoard * 3)].grid[row % 3][col % 3] = -1;
-      grid[colBoard + rowBoard * 3].active = false;
+    for (let i = 0; i < 9; i++) {
 
-      if (grid[col % 3 + (row % 3) * 3].winner == 0) {
-
-        grid[col % 3 + (row % 3) * 3].active = true;
-        turn = 1;
-
-      } else {
-
-        turn = 0;
-
-      }
+        if (grid[i].active) count++;
 
     }
 
-  }
-  
-  checkTriplet(colBoard + rowBoard * 3);
-  
-  let count = 0;
-  
-  for (let i = 0; i < 9; i++) {
-    
-   if (grid[i].active) count++; 
-    
-  }
-  
-  if (count == 0) {
-    
-   if (turn == 2) turn = 3;
-    else if (turn == 1) turn = 0;
-    
-  }
+    if (count == 0) {
+
+        if (turn == 2) turn = 3;
+        else if (turn == 1) turn = 0;
+
+    }
 
 }
 
@@ -204,7 +204,7 @@ function mouseClicked() {
 function checkTriplet(index) {
 
 
-  grid[index].checkWin();
+    grid[index].checkWin();
 
 
 }
@@ -212,64 +212,64 @@ function checkTriplet(index) {
 
 function checkGameEnd() {
 
-  for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
 
-    let rowSum = 0;
+        let rowSum = 0;
 
-    for (let j = 0; j < 3; j++) {
+        for (let j = 0; j < 3; j++) {
 
-      rowSum += grid[j + (i % 3) * 3].winner;
+            rowSum += grid[j + (i % 3) * 3].winner;
 
-    }
+        }
 
-    if (rowSum == 3)
-      endWin = 1;
+        if (rowSum == 3)
+            endWin = 1;
 
-    else if (rowSum == -3)
-      endWin = -1;
-
-  }
-
-
-  for (let i = 0; i < 3; i++) {
-
-    let colSum = 0;
-
-    for (let j = 0; j < 3; j++) {
-
-      colSum += grid[i + (j % 3) * 3].winner;
+        else if (rowSum == -3)
+            endWin = -1;
 
     }
 
-    if (colSum === 3)
-      endWin = 1;
 
-    else if (colSum === -3)
-      endWin = -1;
-  }
+    for (let i = 0; i < 3; i++) {
+
+        let colSum = 0;
+
+        for (let j = 0; j < 3; j++) {
+
+            colSum += grid[i + (j % 3) * 3].winner;
+
+        }
+
+        if (colSum === 3)
+            endWin = 1;
+
+        else if (colSum === -3)
+            endWin = -1;
+    }
 
 
-  if (grid[0].winner + grid[4].winner + grid[8].winner == 3)
-    endWin = 1;
+    if (grid[0].winner + grid[4].winner + grid[8].winner == 3)
+        endWin = 1;
 
-  else if (grid[0].winner + grid[4].winner + grid[8].winner == -3)
-    endWin = -1;
+    else if (grid[0].winner + grid[4].winner + grid[8].winner == -3)
+        endWin = -1;
 
-  if (grid[6].winner + grid[4].winner + grid[2].winner == 3)
-    endWin = 1;
+    if (grid[6].winner + grid[4].winner + grid[2].winner == 3)
+        endWin = 1;
 
-  else if (grid[6].winner + grid[4].winner + grid[2].winner == -3)
-    endWin = -1;
+    else if (grid[6].winner + grid[4].winner + grid[2].winner == -3)
+        endWin = -1;
 
 }
 
 
 function keyPressed() {
 
-  if (key == 'd') {
+    if (key == 'd') {
 
-    print(grid, "\n", turn);
+        print(grid, "\n", turn);
 
-  }
+    }
 
 }
--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,78 +1,78 @@
 function TicTacToe() {
 
-  this.active = false;
-  this.winner = 0;
-  
-  this.grid = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-  ];
-  
-  
-  this.checkWin = function() {
-    
-    for(let i = 0; i < 3; i++){
-      
-        let rowSum = 0;
-      
-        for(let j = 0; j < 3; j++){
-          
-            rowSum += this.grid[i][j];
-          
+    this.active = false;
+    this.winner = 0;
+
+    this.grid = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
+
+
+    this.checkWin = function () {
+
+        for (let i = 0; i < 3; i++) {
+
+            let rowSum = 0;
+
+            for (let j = 0; j < 3; j++) {
+
+                rowSum += this.grid[i][j];
+
+            }
+
+            if (rowSum == 3)
+                this.winner = 1;
+
+            else if (rowSum == -3)
+                this.winner = -1;
+
         }
-      
-        if(rowSum == 3)
-            this.winner = 1;
-      
-        else if(rowSum == -3)
-            this.winner = -1;
-      
-    }
-    
 
-    for(let i = 0; i < 3; i++){
-      
-        let colSum = 0;
-      
-        for(let j = 0; j < 3;j++){
-          
-            colSum += this.grid[j][i];
-          
+
+        for (let i = 0; i < 3; i++) {
+
+            let colSum = 0;
+
+            for (let j = 0; j < 3; j++) {
+
+                colSum += this.grid[j][i];
+
+            }
+
+            if (colSum === 3)
+                this.winner = 1;
+
+            else if (colSum === -3)
+                this.winner = -1;
         }
-      
-        if(colSum === 3)
+
+
+        if (this.grid[0][0] + this.grid[1][1] + this.grid[2][2] === 3)
             this.winner = 1;
-      
-        else if(colSum === -3)
+
+        else if (this.grid[0][0] + this.grid[1][1] + this.grid[2][2] === -3)
             this.winner = -1;
-    }
-    
 
-    if(this.grid[0][0] + this.grid[1][1] + this.grid[2][2] === 3)
-        this.winner = 1;
-    
-    else if(this.grid[0][0] + this.grid[1][1] + this.grid[2][2] === -3)
-        this.winner = -1;
+        if (this.grid[2][0] + this.grid[1][1] + this.grid[0][2] === 3)
+            this.winner = 1;
 
-    if(this.grid[2][0] + this.grid[1][1] + this.grid[0][2] === 3)
-        this.winner = 1;
-    
-    else if(this.grid[2][0] + this.grid[1][1] + this.grid[0][2] === -3)
-        this.winner = -1;
-    
-    
-    
-    
-    
-    
-    if (this.winner != 0) {
-      
-      this.active = false;
-      checkGameEnd();
-      
+        else if (this.grid[2][0] + this.grid[1][1] + this.grid[0][2] === -3)
+            this.winner = -1;
+
+
+
+
+
+
+        if (this.winner != 0) {
+
+            this.active = false;
+            checkGameEnd();
+
+        }
+
     }
-    
-  }
 
 }
