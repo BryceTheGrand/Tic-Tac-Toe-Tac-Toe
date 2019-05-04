2e9fd8b754ffff8950aa0dc5f62249a9e97f0152
BryceTheGrand <whatkindofbunny@gmail.com>
2019-05-04 16:24:05 -0400

    Beautified and changed to ES6 Class notation for TicTacToe obj

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,78 +1,69 @@
-function TicTacToe() {
+class TicTacToe {
 
-    this.active = false;
-    this.winner = 0;
+	constructor() {
 
-    this.grid = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ];
+		this.active = false;
+		this.winner = 0;
+		this.grid = [
+			[0, 0, 0],
+			[0, 0, 0],
+			[0, 0, 0]
+		];
 
+	}
 
-    this.checkWin = function () {
+	checkWin() {
 
-        for (let i = 0; i < 3; i++) {
+		for (let i = 0; i < 3; i++) {
 
-            let rowSum = 0;
+			let rowSum = 0;
 
-            for (let j = 0; j < 3; j++) {
+			for (let j = 0; j < 3; j++) {
 
-                rowSum += this.grid[i][j];
+				rowSum += this.grid[i][j];
 
-            }
+			}
 
-            if (rowSum == 3)
-                this.winner = 1;
+			if (rowSum == 3)
+				this.winner = 1;
+			else if (rowSum == -3)
+				this.winner = -1;
 
-            else if (rowSum == -3)
-                this.winner = -1;
+		}
 
-        }
+		for (let i = 0; i < 3; i++) {
 
+			let colSum = 0;
 
-        for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
 
-            let colSum = 0;
+				colSum += this.grid[j][i];
 
-            for (let j = 0; j < 3; j++) {
+			}
 
-                colSum += this.grid[j][i];
+			if (colSum === 3)
+				this.winner = 1;
+			else if (colSum === -3)
+				this.winner = -1;
 
-            }
+		}
 
-            if (colSum === 3)
-                this.winner = 1;
+		if (this.grid[0][0] + this.grid[1][1] + this.grid[2][2] === 3)
+			this.winner = 1;
+		else if (this.grid[0][0] + this.grid[1][1] + this.grid[2][2] === -3)
+			this.winner = -1;
 
-            else if (colSum === -3)
-                this.winner = -1;
-        }
+		if (this.grid[2][0] + this.grid[1][1] + this.grid[0][2] === 3)
+			this.winner = 1;
+		else if (this.grid[2][0] + this.grid[1][1] + this.grid[0][2] === -3)
+			this.winner = -1;
 
+		if (this.winner != 0) {
+			this.active = false;
+			checkGameEnd();
 
-        if (this.grid[0][0] + this.grid[1][1] + this.grid[2][2] === 3)
-            this.winner = 1;
+		}
 
-        else if (this.grid[0][0] + this.grid[1][1] + this.grid[2][2] === -3)
-            this.winner = -1;
-
-        if (this.grid[2][0] + this.grid[1][1] + this.grid[0][2] === 3)
-            this.winner = 1;
-
-        else if (this.grid[2][0] + this.grid[1][1] + this.grid[0][2] === -3)
-            this.winner = -1;
-
-
-
-
-
-
-        if (this.winner != 0) {
-
-            this.active = false;
-            checkGameEnd();
-
-        }
-
-    }
+	}
 
 }
